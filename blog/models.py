72ed2f179dd9d142231fa04bbf0881b065fdfbbb
roijan <blacklight070@gmail.com>
2019-12-16 07:42:17 +0200

    Portfolio Websit

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,21 @@ class Blog(models.Model):
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
 
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		txt = ""
+		words = self.body.split()
+		for i in range(10):
+			if i >= len(words):
+				break
+			txt += words[i]+' '
+		if i < len(words):
+			txt+='....'
+		return txt
+
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
+
